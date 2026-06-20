61215fd360a0017ddce8c20e6d7850a346f88949
nirjon198 <mahdi72224@gmail.com>
2026-06-20 12:29:15 +0600

    string 3

--- part1.js
+++ part1.js
@@ -16,6 +16,11 @@ console.log(guestList);
 
 //what if we use single or double quote for multiple line
 // let credit="mohsin mama,
-// mami,shawon,fiaz";  
+// mami,shawon,fiaz";
 //here we get compile time error.the solution is \n
 // console.log(credit);
+
+//now we will see how to add backslash or apostrophe in string line
+
+let paragraph='It\'s a beautiful rainy day.in this beautiful weather i would like to drink malai cha\\black coffee.black coffee should be \"without sugar\"';
+console.log(paragraph);
