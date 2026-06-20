203affaebe97157855083a082ee20f33f34c2ba2
nirjon198 <mahdi72224@gmail.com>
2026-06-20 12:37:49 +0600

    length

---
+++ part2.js
@@ -0,0 +1,8 @@
+//now we will learn about string length
+let a=`learn`;
+console.log(a.length);
+let b=`john
+mathew
+david`;
+//here new line is also a character.so the output is 17,however there are 15 alphabet.
+console.log(b.length);
